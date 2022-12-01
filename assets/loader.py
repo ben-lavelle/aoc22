b160747f75047badb7515dcac4e9dc2953b07327
ben-lavelle <benjamin.lavelle@cabinetoffice.gov.uk>
2022-12-01 16:37:23 +0000

    Fix for 2022; now need 3 cookies

--- assets/loader.py
+++ assets/loader.py
@@ -1,4 +1,4 @@
-from assets.config import USER_SESSION_ID
+from assets.config import USER_SESSION_ID, _GA, _GID
 from requests import get
 import os
 
@@ -8,13 +8,18 @@ def gather_day(n: int, overwrite: bool=False) -> None:
     if os.path.exists(path) and not overwrite:
         print(f'{path} exists for day {n}. Skipping.')
         return
-    response = get(f'https://adventofcode.com/2020/day/{n}/input',
-                   cookies={'session': USER_SESSION_ID}
-                   )
+    response = get(f'https://adventofcode.com/2022/day/{n}/input',
+                   cookies={'session': USER_SESSION_ID,
+                            '_GA': _GA,
+                            '_GID': _GID
+                           }
+               )
     if response.ok:
         with open(path, 'w') as f:
             f.write(response.text)
-    print(f'Day {n} input downloaded to {path}.')
+        print(f'Day {n} input downloaded to {path}.')
+    else:
+        print(f'Day {n} request failed.')
 
 def input_day(n: int) -> str:
     assert 1 <= int(n) <= 25, ValueError 
