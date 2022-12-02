50c04a5e4fe64de137b7ee0fbfbdf32344dea39d
ben-lavelle <benjamin.lavelle@cabinetoffice.gov.uk>
2022-12-02 12:24:49 +0000

    Add in part 2 and change example

--- 02-rps.py
+++ 02-rps.py
@@ -6,11 +6,13 @@ gather_day(2)
 with open(input_day(2), 'r') as f:
     rps_strategy = [(l[0], l[2]) for l in f.readlines() if len(l)>1]
 
-print(rps_strategy[3])
+eg = ('A', 'Y')
+
+beats = {'A': 'Y', 'B': 'Z', 'C':'X'}
+draws = {'A': 'X', 'B': 'Y', 'C':'Z'}
+loses = {'A': 'Z', 'B': 'X', 'C':'Y'}
 
 def judge_rps(opp, you)-> int:
-    beats = {'A': 'Y', 'B': 'Z', 'C':'X'}
-    draws = {'A': 'X', 'B': 'Y', 'C':'Z'}
     yours = {'X': 1  , 'Y': 2  , 'Z': 3 }
     return (  6 * int(you==beats[opp])
             + 3 * int(you==draws[opp])
@@ -18,8 +20,21 @@ def judge_rps(opp, you)-> int:
 
 scores = starmap(judge_rps, rps_strategy)
 
-print(f'For e.g. {rps_strategy[3]} you score {judge_rps(*rps_strategy[3])}')
+print(f'For e.g. {eg} you score {judge_rps(*eg)}')
 print(f'Overall score: {sum(scores):,}!')
+
+print('Adjusting strategy...')
+
+def set_response(opp, outcome)-> tuple:
+    fix_to = {'X': loses, 'Y': draws, 'Z': beats}
+    return (opp, fix_to[outcome][opp])
+
+print(f'For e.g. {eg} we transform {set_response(*eg)} '
+      f'and get {judge_rps(*set_response(*eg))}.')
+
+updated_scores = starmap(judge_rps, starmap(set_response, rps_strategy))
+print(f'New score: {sum(updated_scores):,}!')
+
 
 
 
